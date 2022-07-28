4eea6db7593e61acb2bcd790650dcd7e35482451
Андрей Куранов <andreumironov0@gmail.com>
2022-07-28 20:23:44 +0300

    task_01: Урок 1 задание 1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <div class="container-xl">
+    <div class="p-4">
+      <h1 class="text-center text-dark">{{ message }}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +11,7 @@
   export default {
     data() {
       return {
-        
+        message: 'Hello from Vue App',
       }
     },
   }
